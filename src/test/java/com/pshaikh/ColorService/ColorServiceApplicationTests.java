766f41eed729c72adeab4238f0c8b1b8f1ce9051
Karina Heinick <karina@Karinas-MBP.fritz.box>
2020-06-08 16:38:30 +0200

    create project

---
+++ src/test/java/com/pshaikh/ColorService/ColorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pshaikh.ColorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
