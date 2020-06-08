67471e03c982b085b1ffcd782a8cee839e804830
Karina Heinick <karina@Karinas-MBP.fritz.box>
2020-06-08 16:42:20 +0200

    rename package

--- src/main/java/com/pshaikh/color_service/ColorServiceApplication.java
+++ src/main/java/com/pshaikh/color_service/ColorServiceApplication.java
@@ -1,4 +1,4 @@
-package com.pshaikh.ColorService;
+package com.pshaikh.color_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
